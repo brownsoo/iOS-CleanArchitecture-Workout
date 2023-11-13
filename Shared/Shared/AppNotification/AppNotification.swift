f9e15d6485f6f8501dd545821266adf304c759e6
brownsoo <hansune@me.com>
2023-11-13 21:04:14 +0900

    [feat] 알림센터에 좋아요 변경건 알림 추가

---
+++ Shared/Shared/AppNotification/AppNotification.swift
@@ -0,0 +1,20 @@
+//
+//  AppNotification.swift
+//  Shared
+//
+//  Created by hyonsoo on 11/13/23.
+//
+
+import Foundation
+
+public class AppNotification {
+    public static let FavoritesChanged = Notification.Name("FavoritesChanged")
+    
+    public static let shared = AppNotification()
+    
+    private init() {}
+    
+    public func notifyFavoritesChanged() {
+        NotificationCenter.default.post(name: AppNotification.FavoritesChanged, object: nil)
+    }
+}
